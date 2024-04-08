c2ab278c72f4d39a4e34a84c15c4e8cf3d05818c
H-Len <8978475+H-Len@users.noreply.github.com>
2024-04-08 18:04:16 +0900

    ParentNode class added; (WIP)

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,12 +32,41 @@ class LeafNode(HTMLNode):
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props, value=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        str_rep = ""
+        if self.tag == None:
+            raise ValueError('no value present')
+        if self.children == None:
+            raise ValueError('no children present')
+        # if self.props == None:
+        #     self.props = ""
+        for child in self.children:
+            str_rep += child.to_html()
+        return  f"<{self.tag}{self.props_to_html()}>{str_rep}</{self.tag}>"
+
 def main():
     a_node = HTMLNode("<a>", value = "Hello, there", children = None, props = {"href": "https://www.google.com", "target": "_blank"})
     
     # print(a_node.props_to_html())
     # print(a_node.__repr__())
     # print(a_node.props.keys())
-    leaf_node1 = LeafNode("a", "Hi", {"href": "try.me.com", "target": "_blank"})
-    print(leaf_node1.to_html())
+    # leaf_node1 = LeafNode("a", "Hi", {"href": "try.me.com", "target": "_blank"})
+    # print(leaf_node1.to_html())
+
+    node = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+    None
+    )
+
+    print(node.to_html())
 main()
