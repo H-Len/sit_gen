bea4c27be56797c6a2e852063c18e1d5f8d89886
H-Len <8978475+H-Len@users.noreply.github.com>
2024-04-08 18:06:26 +0900

    code cleanup (WIP)

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,17 +33,15 @@ class LeafNode(HTMLNode):
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag, children, props, value=None):
+    def __init__(self, tag, children, props):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
         str_rep = ""
         if self.tag == None:
-            raise ValueError('no value present')
+            raise ValueError('no tag present')
         if self.children == None:
             raise ValueError('no children present')
-        # if self.props == None:
-        #     self.props = ""
         for child in self.children:
             str_rep += child.to_html()
         return  f"<{self.tag}{self.props_to_html()}>{str_rep}</{self.tag}>"
