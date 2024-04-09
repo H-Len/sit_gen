0ac7966322fbcc364e98dd6622aa463576756e9f
H-Len <8978475+H-Len@users.noreply.github.com>
2024-04-09 17:23:39 +0900

    textnode to htmlnode method created in textnode; text placed in test_textnode file

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,9 @@
+from htmlnode import LeafNode
+
 class TextNode:
     def __init__(self, text, text_type, url = None):
-        self.text = text,
-        self.text_type = text_type,
+        self.text = text
+        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
@@ -12,5 +14,29 @@ class TextNode:
         )
     
     def __repr__(self):
-        # TextNode(TEXT, TEXT_TYPE, URL)
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def text_node_to_html_node(text_node):
+        if text_node.text_type == "text":
+            return LeafNode(text_node.text)
+        elif text_node.text_type == "bold":
+            return LeafNode("b", text_node.text)
+        elif text_node.text_type == "italic":
+            return LeafNode("i", text_node.text)
+            # text_type_italic: "i" tag, text
+        elif text_node.text_type == "code":
+            return LeafNode(text_node.tag, text_node.text)
+        elif text_node.text_type == "link":
+            props = {"href": text_node.url}
+            return LeafNode("a", text_node.text, props)
+        elif text_node.text_type == "image":
+            props_src_alt = {"src": text_node.url,
+                             "alt": text_node.text}
+            return LeafNode("img", '', props_src_alt)
+        else:
+            raise Exception("Not of the available text_types")
+        
+
+trial_1 = TextNode(text="something", text_type="bold")
+
+print(TextNode.text_node_to_html_node(trial_1))
