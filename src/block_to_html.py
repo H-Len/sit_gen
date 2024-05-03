ca408f10c8e680854bf5838bd878c980637d7ae6
H-Len <8978475+H-Len@users.noreply.github.com>
2024-05-02 19:44:30 -0700

    block to html WIP

--- src/block_to_html.py
+++ src/block_to_html.py
@@ -15,25 +15,29 @@ def blocktype_to_html(blocks):
     if block_to_block_type(blocks) == block_type_heading:
         # Headings should be surrounded by a <h1> to <h6> tag, depending on the number of # characters.
         # delimiter = '#'
-        count = 0
-        if count('#') == 2:
-            split_block = blocks.split('#')
-            return f"<h1>{split_block}</h1>"
-        elif count('##') == 2:
-            split_block = blocks.split('##')
-            return f"<h2>{split_block}</h2>"
-        elif count('###') == 2:
-            split_block = blocks.split('###')
-            return f"<h3>{split_block}</h3>"
-        elif count('####') == 2:
-            split_block = blocks.split('####')
-            return f"<h4>{split_block}</h4>"
-        elif count('#####') == 2:
-            split_block = blocks.split('#####')
-            return f"<h5>{split_block}</h5>"
-        elif count('######') == 2:
-            split_block = blocks.split('######')
-            return f"<h6>{split_block}</h6>"
+        if block_to_block_type == block_type_heading:
+            count = 0
+            for char in blocks[0:6]:
+                if char == '#':
+                    count += 1
+            if count == 1 and blocks[1] == ' ':
+                split_block = blocks.split('#')
+                return f"<h1>{split_block[0: -1]}</h1>"
+            elif count == 2 and blocks[2] == ' ':
+                split_block = blocks.split('##')
+                return f"<h2>{split_block[1: -2]}</h2>"
+            elif count == 3 and blocks[3] == ' ':
+                split_block = blocks.split('###')
+                return f"<h3>{split_block[2: -3]}</h3>"
+            elif count == 4 and blocks[4] == ' ':
+                split_block = blocks.split('####')
+                return f"<h4>{split_block[3: -4]}</h4>"
+            elif count == 5 and blocks[5] == ' ':
+                split_block = blocks.split('#####')
+                return f"<h5>{split_block[4: -5]}</h5>"
+            elif count == 6 and blocks[6] == ' ':
+                split_block = blocks.split('######')
+                return f"<h6>{split_block[5: -6]}</h6>"
         
     elif block_to_block_type(blocks) == block_type_code:
         # Code blocks should be surrounded by a <code> tag nested inside a <pre> tag.
@@ -41,10 +45,9 @@ def blocktype_to_html(blocks):
         return f"<pre><code>{stript_blocks}</code></pre>"
     
     elif block_to_block_type(blocks) == block_type_quote:
-        #tentative skeleton:
         # Quote blocks should be surrounded by a <blockquote> tag.
-        quote_string = '>hello'
-        return f"<blockquote>{quote_string}</blockquote>"
+        quote_string = '> hello'
+        return f"<blockquote> {quote_string[2:]}</blockquote>"
 
     elif block_to_block_type(blocks) == block_type_unordered:
         # Unordered list blocks should be surrounded by a <ul> tag, and each list item should be surrounded by a <li> tag.
@@ -64,8 +67,9 @@ def blocktype_to_html(blocks):
         2. good afternoon
         3. good night'''
         new_ol = ['<ol>']
-        split_ol = ol_string.split(re.match(r"^\d+\."))
-        i = 1
+        split_ol = ol_string.split(re.match(r"^\d\."))
+        print(re.match(r"^\d\."))
+        # i = 1
         for line in split_ol:
             new_ul.append(f'\n<li>{line}</li>')
         ''.join(new_ul)
--- src/test_block_to_html.py
+++ src/test_block_to_html.py
@@ -1,4 +1,5 @@
 import unittest
+import re 
 
 from block_to_html import blocktype_to_html
 # block = '* smile'
@@ -18,7 +19,7 @@ class TestInlineMarkdown(unittest.TestCase):
     def test_quote(self, quote_test):
         quote_test = '''> the beginning
 > is here'''
-        expected_quote = '<blockquote> the beginning\n is here</blockquote>'
+        expected_quote = '<blockquote>the beginning\n is here</blockquote>'
         print(blocktype_to_html(quote_test), expected_quote)
     def test_ul(self):
         unordered_test = '''- pack
@@ -27,20 +28,20 @@ class TestInlineMarkdown(unittest.TestCase):
     <li>pack</li>
     <li>move</li>
 </ul>'''
-        print(blocktype_to_html(unordered_test))
+        return self.assertEqual(blocktype_to_html(unordered_test), expected_ul)
     def test_ol(self):
         ordered_test = '''1. wake up
 2. smile'''
-        espected_ol = '''<ol>
-            <li>wake up</li>
-            <li>smile</li>
-        </ol>'''
-        print(blocktype_to_html(ordered_test))
+        expected_ol = '''<ol>
+    <li>wake up</li>
+    <li>smile</li>
+</ol>'''
+        return self.assertEqual(blocktype_to_html(ordered_test), expected_ol)
     def test_paragraph(self):
         paragraph_test = '''I originally thought something for the first time and it was just me, myself, who thought it.
         But, then I told you, and you thought about it, too.
-        Still original?
-        '''
+        Still original?'''
+
         expected_p = '''<p>I originally thought something for the first time and it was just me, myself, who thought it.
         But, then I told you, and you thought about it, too.
         Still original?</p>'''
