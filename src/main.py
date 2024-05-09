762195cc98eb957e01daacf8ac0730654e421b50
H-Len <8978475+H-Len@users.noreply.github.com>
2024-05-08 21:09:26 -0700

    beginning to write generate page funtion (WIP)

--- src/main.py
+++ src/main.py
@@ -29,14 +29,25 @@ def extract_title(markdown):
     raise Exception('No H1 header')
 
 
+def generate_page(from_path, template_path, dest_path):
+    '''    Print a message to the console that says something like "Generating page from from_path to dest_path using template_path".
+    -- Read the markdown file at from_path and store the contents in a variable.
+    -- Read the template file at template_path and store the contents in a variable.
+    -- Use your markdown_to_html_node function and .to_html() method to convert the markdown file to HTML.
+    -- Use the extract_title function to grab the title of the page.
+    -- Replace the {{ Title }} and {{ Content }} placeholders in the template with the HTML and title you generated.
+    -- Write the new HTML to a file at dest_path. Be sure to create any necessary directories if they don't exist.'''
 
-    
-    
+    print(f'Generating page from {from_path} to {dest_path} using {template_path}')
 
-def main():
-    copy_dir_rec('./static', './public')
-
-    md_file = open('../content/index.md')
+    md_file = open(template_path)
     md_text = md_file.read()
     extract_title(md_text)
+
+
+    
+def main():
+    copy_dir_rec('./static', './public')
+    generate_page('../content/index.md', '../template.html', '../public/index.md')
+    
 main()
